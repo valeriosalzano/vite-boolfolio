9e0f86bcd2df8dc18d23e737bbda9f8cc6d7c58f
Valerio Salzano <vlr.slzn@gmail.com>
2023-05-30 16:44:25 +0200

    milestone 3 done - new project created - axios installed - added a header with a navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <script>
+  import AppHeader from './components/AppHeader.vue';
+
+  export default {
+    components: {
+      AppHeader,
+    }
+  }
 
 </script>
 
 <template>
+  <body>
+    <AppHeader></AppHeader>
 
+  </body>
 </template>
 
 <style lang="scss">
 
   @use './styles/general.scss';
-  
+
 </style>
